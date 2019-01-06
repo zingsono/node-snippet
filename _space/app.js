322ebbc9c9a7ad4684d2c833006db72cf4a7ed31
Zengs <zingsono@gmail.com>
2019-01-06 23:51:17 +0800

    _space

--- _space/app.js
+++ _space/app.js
@@ -5,7 +5,8 @@ app.use(express.static('public'))
 app.all('/space/:rt', function (req, res) {
     let runtime = `./runtime/${req.params.rt}`
     delete require.cache[require.resolve(runtime)]
-    res.send(require(runtime)(req, res))
+    let rs = require(runtime)(req, res)
+    res.send(rs)
 })
 app.listen(30625)
 console.log('Server http://localhost:30625/')
---
+++ _space/runtime/u_file.js
@@ -0,0 +1,19 @@
+/**
+ * 文件读写
+ */
+
+const fs = require('fs')
+
+module.exports = function () {
+    return {
+        /**
+         * 内容写入文件
+         * @param content
+         * @param file
+         */
+        writeFile(content, file){
+
+
+        }
+    }
+}
