248cc023c8745c61196e0a6aab9facf7fc22cda9
Zengs <zingsono@gmail.com>
2019-03-18 21:49:20 +0800

    nwjs

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,15 +17,18 @@ module.exports = {
         // 禁用未声明的变量
         'no-undef': 'off',
         // 禁止在语句末尾使用分号
-        'semi': ['error','never'],
-        // 强制在逗号周围使用空格
-        'comma-spacing':['error',{ "before": false, "after": true }],
+        'semi': ['error', 'never'],
+        // 强制在逗号后使用空格
+        'comma-spacing': ['error', {
+            "before": false,
+            "after": true
+        }],
         // 限定单引号字符串
         'quotes': ['error', 'single'],
         // 限定代码缩进4个空格
         'indent': ['error', 4],
         // 强制使用 Unix 换行符： \n
-        'linebreak-style':['error','unix'],
+        'linebreak-style': ['error', 'unix'],
         // 允许使用console
         'no-console': 'off'
     },
