9cce64b0f2f1d8c3645fb0ee2cd3adafc3c2a7b0
Zengs <zingsono@gmail.com>
2019-05-17 17:25:14 +0800

    react

--- reactjs/react-app/src/pages/Child.tsx
+++ reactjs/react-app/src/pages/Child.tsx
@@ -1,37 +1,36 @@
-import React, {forwardRef, Ref, useImperativeHandle, useState} from 'react';
+import React, {FC, forwardRef, Ref, useImperativeHandle, useState} from 'react';
 
 //import {MyContext} from './Parent'
 
 export interface Props {
     children: string;
     onPlus: Function;
-    onChildMethod?:Function;
-    ref:Ref<Up> ;
+    onChildMethod?: Function;
+    ref: Ref<Up>;
 }
 
 export interface Up {
-    updateUp:Function;
+    updateUp: Function;
 }
 
-export const Child: React.FC<Props> = forwardRef((props: Props,ref:Ref<Up>) => {
+export const Child: FC<Props> = forwardRef((props: Props, ref: Ref<Up>) => {
     console.log('ChildProps=', JSON.stringify(props))
-    const [up,setUp] = useState(0);
-
-
-    useImperativeHandle(ref,()=>({
-        updateUp:(p:number)=>{ setUp(p) }
+    const [up, setUp] = useState(0);
+    useImperativeHandle(ref, () => ({
+        updateUp: (p: number) => {
+            console.log('updateUp', p)
+            setUp(p)
+        }
     }))
-
-
-    if(props.onChildMethod){
-        props.onChildMethod((up:number)=>{ setUp(up) })
-    }
 
     return (
         <div className="Child">
-            <button onClick={()=>{props.onPlus()}}>Child Button</button>
+            <button onClick={() => {
+                props.onPlus()
+            }}>Child Button
+            </button>
             <p>Child props.children ={props.children}</p>
             <p>up={up}</p>
         </div>
-    );
+    )
 })
--- reactjs/react-app/src/pages/Parent.tsx
+++ reactjs/react-app/src/pages/Parent.tsx
@@ -1,35 +1,31 @@
-import React, {RefObject, useRef, useState} from 'react';
+import React, {RefObject, useState} from 'react';
 
-import {Child,Up} from './Child'
+import {Child, Up} from './Child'
 
-export interface Props{
-    children:string;
-    label:string;
+export interface Props {
+    children: string;
+    label: string;
 }
 
 export const Parent: React.FC<Props> = (props: Props) => {
-    const [count,setCount] = useState(0);
-    console.log('Parent.props = ',JSON.stringify(props))
+    const [count, setCount] = useState(0);
+    console.log('Parent.props = ', JSON.stringify(props))
 
 
-    let plus = ()=>{
-        setCount(count+1)
+    let plus = () => {
+        setCount(count + 1)
     }
 
-   /* let onChildMethod = (childMethod:Function)=>{
-        childMethod(count)
-    }*/
-
-
-   let refChild:RefObject<Up> =  React.createRef<Up>()
+    let refChild: RefObject<Up> = React.createRef<Up>()
 
     return (
         <div className="Parent">
             <p>Count: {count}</p>
-            <button onClick={plus}>Parent Button</button><br/>
-            <button onClick={ refChild.current&&refChild.current.updateUp(count) }>Update Child Button</button>
+            <button onClick={plus}>Parent Button</button>
+            <br/>
+            <button onClick={()=>{refChild.current && refChild.current.updateUp(count)}}>Update Child Button</button>
             <div>Parent props.children = {props.children}</div>
             <Child {...props} onPlus={plus} ref={refChild}>childText</Child>
         </div>
-    );
+    )
 }
